cff1a1029e278b3f9ed696d2ca1a6f964da7fcf5
Jonakss <jona.correapc@gmail.com>
2020-09-26 00:48:25 -0300

    Commented State manager

--- src/State.cpp
+++ src/State.cpp
@@ -2,5 +2,5 @@
 #include "../headers/State.hpp"
 
 void State::changeState(StateManager* sm, State* s){
-  sm->changeState(s);
+  //sm->changeState(s);
 };
--- src/StateManager.cpp
+++ src/StateManager.cpp
@@ -2,22 +2,22 @@
 
 void StateManager::changeState(char* id){
   int i = 0;
-  bool e = false
+  bool e = false;
   while ((i<states.size()) && (e)) {
-    i++;
-    if(states[i]==id){
-      cState = states[i];
-      e = true;
-    }
+  //  i++;
+   // if(states[i]==id){
+    //  cState = states[i];
+     // e = true;
+    //}
   };
 };
 
 void StateManager::pushState(State* s){
-  this->states.push(s);
+  //this->states.push(s);
 };
 
 void StateManager::popState(State* s){
-  this->states.pop(s);
+  //this->states.pop(s);
 };
 
 void StateManager::draw(sf::RenderTarget* rt){
